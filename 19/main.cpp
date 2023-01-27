da13e96a8014045ad51cb42b3c979140daa770d8
alexander laas <laas.alexander@gmail.com>
2023-01-27 23:38:25 +0100

    19.1 final

--- 19/main.cpp
+++ 19/main.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 
 #include "common.hpp"
+#include "stopwatch.h"
 
 using Vec = std::vector<int>;
 #define ORE 0
@@ -16,111 +17,103 @@ using Vec = std::vector<int>;
 #define C_GEO_ORE 5
 #define C_GEO_OBS 6
 
-#define TIME_CAP 24
+#define TIME_CAP 24  // 1
+// #define TIME_CAP 32  // 2
 
 class Strategy {
  public:
-  Strategy() {}
-  Strategy(Vec blueprint, int minutes)
-      : blueprint(blueprint), minutes(minutes) {}
+  Strategy(Vec blueprint) : blueprint(blueprint) {}
 
-  Strategy(Vec blueprint, Vec setup, Vec resources, int minutes)
-      : blueprint(blueprint),
-        setup(setup),
-        resources(resources),
-        minutes(minutes) {}
-
-  std::vector<Strategy> spawn() {
-    std::vector<Strategy> spawned;
-    spawned.push_back(spawn_ore());
-    spawned.push_back(spawn_clay());
-    if (setup.at(CLAY) > 0) spawned.push_back(spawn_obsidian());
-    if (setup.at(OBS) > 0) spawned.push_back(spawn_geode());
-    return spawned;
-  }
-
-  Vec setup = {1, 0, 0, 0};
-  Vec resources = {0, 0, 0, 0};
+  int run() { return build_new({1, 0, 0, 0}, {0, 0, 0, 0}, 0, 0); }
   Vec blueprint = {0, 0, 0, 0, 0, 0, 0};
-  int minutes;
 
  private:
-  int calc_add_time(int resource_ind, int cost_ind) {
+  int single_resource_spawn(const Vec setup, const Vec resources, int ind_s,
+                            int ind_r, int ind_c_r, int minutes, int max_g) {
+    if (max_g >= calc_potential(setup, resources, minutes)) {
+      return 0;
+    }
+    int add_minutes = calc_add_time(ind_r, ind_c_r, setup, resources);
+
+    if (minutes + add_minutes > TIME_CAP) {
+      auto updated_resources =
+          increase_resources(minutes - TIME_CAP, setup, resources);
+      return updated_resources.at(GEO);
+    }
+    auto updated_resources = increase_resources(add_minutes, setup, resources);
+    updated_resources.at(ind_r) -= blueprint.at(ind_c_r);
+    auto updated_setup = add_at(ind_s, setup);
+    return build_new(updated_setup, updated_resources, minutes + add_minutes,
+                     max_g);
+  }
+
+  int double_resource_spawn(Vec setup, Vec resources, int ind_s, int ind_r1,
+                            int ind_c_r1, int ind_r2, int ind_c_r2, int minutes,
+                            int max_g) {
+    if (max_g >= calc_potential(setup, resources, minutes) ||
+        setup.at(ind_s - 1) == 0) {
+      return 0;
+    }
+
+    int add_minutes_ore = calc_add_time(ind_r1, ind_c_r1, setup, resources);
+    int add_minutes_clay = calc_add_time(ind_r2, ind_c_r2, setup, resources);
+    int add_minutes = std::max(add_minutes_ore, add_minutes_clay);
+    if (minutes + add_minutes > TIME_CAP) {
+      auto updated_resources =
+          increase_resources(TIME_CAP - minutes, setup, resources);
+      return updated_resources.at(GEO);
+    }
+    auto updated_resources = increase_resources(add_minutes, setup, resources);
+    updated_resources.at(ind_r1) -= blueprint.at(ind_c_r1);
+    updated_resources.at(ind_r2) -= blueprint.at(ind_c_r2);
+    auto updated_setup = add_at(ind_s, setup);
+    return build_new(updated_setup, updated_resources, minutes + add_minutes,
+                     max_g);
+  }
+
+  int build_new(const Vec setup, const Vec resources, int minutes, int max_g) {
+    auto ore = single_resource_spawn(setup, resources, ORE, ORE, C_ORE_ORE,
+                                     minutes, max_g);
+    max_g = std::max(max_g, ore);
+    auto clay = single_resource_spawn(setup, resources, CLAY, ORE, C_CLAY_ORE,
+                                      minutes, max_g);
+    max_g = std::max(max_g, clay);
+    auto obs = double_resource_spawn(setup, resources, OBS, ORE, C_OBS_ORE,
+                                     CLAY, C_OBS_CLAY, minutes, max_g);
+    max_g = std::max(max_g, obs);
+    auto geo = double_resource_spawn(setup, resources, GEO, ORE, C_GEO_ORE, OBS,
+                                     C_GEO_OBS, minutes, max_g);
+    return std::max(max_g, geo);
+  }
+
+  int calc_add_time(int resource_ind, int cost_ind, const Vec setup,
+                    const Vec resources) {
     int diff = std::max(blueprint.at(cost_ind) - resources.at(resource_ind), 0);
     return diff % setup.at(resource_ind) == 0
                ? diff / setup.at(resource_ind) + 1
                : diff / setup.at(resource_ind) + 2;
   }
 
-  Strategy spawn_ore() {
-    int add_minutes = calc_add_time(ORE, C_ORE_ORE);
-    if (minutes + add_minutes > TIME_CAP) {
-      add_minutes = TIME_CAP - minutes;
-      return Strategy(blueprint, setup, increase_resources(add_minutes),
-                      TIME_CAP);
-    }
-    auto updated_resources = increase_resources(add_minutes);
-    updated_resources.at(ORE) -= blueprint.at(C_ORE_ORE);
-    return Strategy(blueprint, add_at(ORE), updated_resources,
-                    minutes + add_minutes);
-  }
-
-  Strategy spawn_clay() {
-    int add_minutes = calc_add_time(ORE, C_CLAY_ORE);
-    if (minutes + add_minutes > TIME_CAP) {
-      add_minutes = TIME_CAP - minutes;
-      return Strategy(blueprint, setup, increase_resources(add_minutes),
-                      TIME_CAP);
-    }
-    auto updated_resources = increase_resources(add_minutes);
-    updated_resources.at(ORE) -= blueprint.at(C_CLAY_ORE);
-    return Strategy(blueprint, add_at(CLAY), updated_resources,
-                    minutes + add_minutes);
-  }
-  Strategy spawn_obsidian() {
-    int add_minutes_ore = calc_add_time(ORE, C_OBS_ORE);
-    int add_minutes_clay = calc_add_time(CLAY, C_OBS_CLAY);
-    int add_minutes = std::max(add_minutes_ore, add_minutes_clay);
-    if (minutes + add_minutes > TIME_CAP) {
-      add_minutes = TIME_CAP - minutes;
-      return Strategy(blueprint, setup, increase_resources(add_minutes),
-                      TIME_CAP);
-    }
-    auto updated_resources = increase_resources(add_minutes);
-    updated_resources.at(ORE) -= blueprint.at(C_OBS_ORE);
-    updated_resources.at(CLAY) -= blueprint.at(C_OBS_CLAY);
-    return Strategy(blueprint, add_at(OBS), updated_resources,
-                    minutes + add_minutes);
-  }
-  Strategy spawn_geode() {
-    int add_minutes_ore = calc_add_time(ORE, C_GEO_ORE);
-    int add_minutes_obs = calc_add_time(OBS, C_GEO_OBS);
-    int add_minutes = std::max(add_minutes_ore, add_minutes_obs);
-    if (minutes + add_minutes > TIME_CAP) {
-      add_minutes = TIME_CAP - minutes;
-      return Strategy(blueprint, setup, increase_resources(add_minutes),
-                      TIME_CAP);
-    }
-    auto updated_resources = increase_resources(add_minutes);
-    updated_resources.at(ORE) -= blueprint.at(C_GEO_ORE);
-    updated_resources.at(OBS) -= blueprint.at(C_GEO_OBS);
-    return Strategy(blueprint, add_at(GEO), updated_resources,
-                    minutes + add_minutes);
-  }
-  Vec add_at(int index) {
+  Vec add_at(int index, const Vec setup) {
     Vec new_vec = {0, 0, 0, 0};
     for (int i = 0; i < 4; ++i) {
       new_vec.at(i) = i == index ? setup.at(i) + 1 : setup.at(i);
     }
     return new_vec;
   }
-  Vec increase_resources(int add_minutes) {
+  Vec increase_resources(int add_minutes, Vec setup, Vec resources) {
     Vec new_vec = {0, 0, 0, 0};
     for (int i = 0; i < 4; ++i) {
       new_vec.at(i) = resources.at(i) + add_minutes * setup.at(i);
     }
     return new_vec;
   }
+
+  int calc_potential(Vec setup, Vec resources, int minutes) {
+    int t = TIME_CAP - minutes;
+
+    return t * (t - 1) / 2 + setup.at(GEO) * t + resources.at(GEO);
+  };
 };
 
 std::vector<int> extractIntegers(std::string line) {
@@ -140,56 +133,37 @@ std::vector<int> extractIntegers(std::string line) {
   return result;
 }
 
-int calc_potential(Strategy s) {
-  int t = TIME_CAP - s.minutes;
-  return t * (t - 1) / 2 + s.setup.at(GEO) * t + s.resources.at(GEO);
-};
-
 int main() {
+  auto sw = StopWatch();
   auto blueprints =
       helpers::parse_input<std::vector<int>>("input.txt", extractIntegers);
 
   Vec points;
   int counter = 0;
-  for (auto &b : blueprints) {
-    std::vector<Strategy> strategies = {Strategy(b, 0)};
-    std::vector<Strategy> ns;
-    Strategy max_s;
-    int max = 0;
-    while (true) {
-      for (auto &s : strategies) {
-        if (s.minutes < TIME_CAP && calc_potential(s) < max) {
-          // throw away.
-        } else if (s.minutes < TIME_CAP) {
-          auto ss = s.spawn();
-          ns.insert(ns.end(), ss.begin(), ss.end());
-        } else {
-          if (s.resources.at(GEO) > max) {
-            max = s.resources.at(GEO);
-            max_s = s;
-          }
-        }
-      }
-      if (ns.size() > 0) {
-        strategies.clear();
-        strategies.insert(strategies.begin(), ns.begin(), ns.end());
-        ns.clear();
-      } else {
-        strategies.clear();
-        ns.clear();
-        break;
-      }
-    }
-    auto score = max_s.resources.at(GEO) * max_s.blueprint.at(ID);
-    points.push_back(score);
-    std::cout << "Current: " << ++counter << std::endl;
-  }
 
+  // 2
+  // blueprints.resize(3);
+  //
+  for (auto &b : blueprints) {
+    auto strategy = Strategy(b);
+    sw.Restart();
+    auto score = strategy.run();
+
+    // 1
+    score *= b.at(ID);
+    // 2
+    points.push_back(score);
+    std::cout << "Current: " << ++counter << " Score: " << score
+              << " Time: " << sw.ElapsedSec() << std::endl;
+  }
   int sum = 0;
+  int product = 1;
   for (auto p : points) {
     sum += p;
+    product *= p;
   }
   std::cout << "1. " << sum << std::endl;
+  // std::cout << "2. " << product << std::endl;
 
   return 0;
 }
